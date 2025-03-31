0dacc91c20a9b83d09dd99cb08c19f1400d6d67d
Rose768510 <roseakouvi.sossoukpe@gmail.com>
2025-03-31 08:20:17 +0000

    commit

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const menu = document.querySelector('#menu');
 const toggleMenu = document.querySelector('#toggle-menu');
-const toggleText = document.querySelector('#toggle-text')
+const toggleText = document.querySelector('#toggle-text');
+const deleteBtn = document.querySelector('#delete');
 
 toggleMenu.addEventListener('click', () => {
     menu.classList.toggle('hidden');
 });
-
+ 
 document.querySelectorAll('.nav-link').forEach((link) => {
     link.addEventListener('click', () => {
         menu.classList.add('hidden');
     })
+});
+
+deleteBtn.addEventListener('click', () => {
+    menu.classList.add('hidden')
 })
